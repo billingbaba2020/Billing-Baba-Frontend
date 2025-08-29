6d25e66174d01b7b1325e3ef19d68b991f4fef5a
bhumikaPandeyCodes <140793724+bhumikaPandeyCodes@users.noreply.github.com>
2025-08-30 05:11:38 +0530

    about page

--- app/(website)/about/about.tsx
+++ app/(website)/about/about.tsx
@@ -1,7 +1,23 @@
+import RatingSection from "@/components/website/rating-section";
+import AboutHero from "./AboutHero";
+import AccountingSoftwareSection from "./AccountingSoftwareSection";
+import AppInfoSection from "./AppInfoSection";
+import BusinessInfoSection from "./BusinessInfoSection";
+import EffortlessBusinessSection from "./EffortlessBusinessSection";
+import RecognitionSection from "./RecognitionSection";
+import TechnologySection from "./TechnologySection";
+
 export default function AboutPage() {
     return (
         <div>
-            <h1>About</h1>
+            <AboutHero />
+            <AccountingSoftwareSection />
+            <TechnologySection />
+            <BusinessInfoSection />
+            <AppInfoSection />
+            <EffortlessBusinessSection />
+            <RecognitionSection />
+            <RatingSection />
         </div>
     )
 }
